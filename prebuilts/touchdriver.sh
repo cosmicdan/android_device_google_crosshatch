10a38390a72ca117caca37d4d824306af0e829ca
CosmicDan <dconnolly.au@gmail.com>
2021-10-05 12:43:00 +1100

    touchdriver.sh logging

--- prebuilts/touchdriver.sh
+++ prebuilts/touchdriver.sh
@@ -1,12 +1,16 @@
 #!/system/bin/sh
 
-sleep 1
+echo "touchdriver.sh starting in 5 seconds..." > /tmp/touchdriver.log
+sleep 5
 fastboot=$(getprop ro.boot.bootreason | cut -d, -F2)
 if [[ $fastboot == "bootloader" || $fastboot == "longkey" || $fastboot == "reboot"  || $fastboot == "recovery" ]]
 then
-	insmod /system/lib64/videobuf2-memops.ko 
-	insmod /system/lib64/videobuf2-vmalloc.ko
-	insmod /system/lib64/heatmap.ko  
-	insmod /system/lib64/ftm5.ko
-	insmod /system/lib64/sec_touch.ko
+	echo "... loading driver because bootreason = ${fastboot}" >> /tmp/touchdriver.log
+	insmod /system/lib64/videobuf2-memops.ko  >> /tmp/touchdriver.log
+	insmod /system/lib64/videobuf2-vmalloc.ko >> /tmp/touchdriver.log
+	insmod /system/lib64/heatmap.ko   >> /tmp/touchdriver.log
+	insmod /system/lib64/ftm5.ko >> /tmp/touchdriver.log
+	insmod /system/lib64/sec_touch.ko >> /tmp/touchdriver.log
+else
+	echo "... SKIPPING driver because bootreason = ${fastboot}" >> /tmp/touchdriver.log
 fi
